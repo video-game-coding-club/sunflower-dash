486b3b150c78b61bb76903990149376832fc296d
Nicolas Bock <nicolasbock@gmail.com>
2018-08-08 15:00:16 +0200

    Update Travis-CI setup

---
+++ lint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e -x
+
+if [[ ${TRAVIS} != "true" ]]; then
+    npm install jshint
+    npm install PrettyCSS
+    npm install js-beautify
+    npm install html-linter
+
+    PATH=${PATH}:node_modules/.bin/
+fi
+
+jshint main.js
+prettycss style.css
+html-linter --config html-linter.json *.html
+js-beautify \
+    --type js \
+    --indent-size 2 \
+    --end-with-newline \
+    --outfile main.js.beautified \
+    main.js
+diff -Naur main.js main.js.beautified
+js-beautify \
+    --type css \
+    --indent-size 4 \
+    --end-with-newline \
+    --outfile style.css.beautified \
+    style.css
+diff -Naur style.css style.css.beautified
+html-beautify \
+    --type html \
+    --indent-size 2 \
+    --indent-inner-html \
+    --end-with-newline \
+    --outfile game.html.beautified \
+    game.html
+diff -Naur game.html game.html.beautified
